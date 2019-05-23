d610d2fe61f088bd7d26596cbe974ec9bc789a99
pledovski <human.pled@gmail.com>
2019-05-23 21:39:31 +0300

    First commit.

---
+++ src/App/WelcomeMessage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function Welcome(props) {
+  return <h1>Hello! It's xChange.best! 
+  The best solution for crypto assets management and exchange operations! </h1>;
+};
---
+++ src/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Welcome from './WelcomeMessage'
+
+
+function App() {
+  return (
+    <Welcome/>
+  );
+}
+
+export default App;
