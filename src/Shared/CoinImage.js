86bfd877179b875abecaafc40239392dbfeaa6a5
pledovski <human.pled@gmail.com>
2019-05-29 00:32:03 +0300

    Coin Spotlight added

--- src/Shared/CoinImage.js
+++ src/Shared/CoinImage.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import styled, {css} from 'styled-components';
 
-export default ({coin, style}) => {
-  return <img
+const CoinImage = styled.img`
+  height: 50px;
+  ${props => props.spotLight && css`
+    height: 200px;
+    display: block;
+    margin: auto;
+  `}
+`
+
+export default ({coin, spotLight}) => {
+  return <CoinImage
+  spotLight={spotLight}
     alt={coin.coinSymbol}
-    style={style || {height: '50px'}}
     src={`http://cryptocompare.com/${coin.ImageUrl}`}
     />;
 }
