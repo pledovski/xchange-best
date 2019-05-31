26061412f41ff9d2463a279988c953b15778f43f
pledovski <human.pled@gmail.com>
2019-05-31 16:37:59 +0300

    Updates

---
+++ precache-manifest.9b488ea66d68e95b6347ea3950537b78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8153e21c5fadebaacf3f90866c6b952c",
+    "url": "/xchange-best/index.html"
+  },
+  {
+    "revision": "27a10f056b3cc850de7b",
+    "url": "/xchange-best/static/css/main.a36ac380.chunk.css"
+  },
+  {
+    "revision": "b95bfc1ec54d4ffd8bf7",
+    "url": "/xchange-best/static/js/2.341f234a.chunk.js"
+  },
+  {
+    "revision": "27a10f056b3cc850de7b",
+    "url": "/xchange-best/static/js/main.8c11a32a.chunk.js"
+  },
+  {
+    "revision": "c50ab445cf96b91cbdd9",
+    "url": "/xchange-best/static/js/runtime~main.f7c6de7d.js"
+  }
+]);
